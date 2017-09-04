4b6978389c1ea476c8e1c692da62f0095b78ade5
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-04 08:53:50 -0400

    added comic books controller

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc; //added this to inherit from MVC
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller //added this to inherit from MVC
+    {
+    }
+}
