3e8cf81d3dab4da01feb0261764c4c6d5d2b3de5
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-06 06:53:39 -0500

    updated detail action methods to use action result types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,15 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller //added this to inherit from MVC
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the world";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+               
+            }
+            return Content("Hello from the world");
+
         }//adding an action method.. performing an action required to prepare a response for request
     }
 }
