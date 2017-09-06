b72f58896429f3ec80bb78192a09df5ac710c9a4
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-06 06:25:12 -0500

    Added Detail Action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller //added this to inherit from MVC
     {
+        public string Detail()
+        {
+            return "Hello from the world";
+        }//adding an action method.. performing an action required to prepare a response for request
     }
 }
