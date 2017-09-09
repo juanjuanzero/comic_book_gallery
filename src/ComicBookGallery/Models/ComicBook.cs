b7595629420b92221e11a4eb369b94ffd61d1445
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-09 16:44:22 -0400

    updated the comic book model to have the cover image file names and the display text

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,24 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }// this is an array of Artist class found under Models
         public bool Favorite { get; set; }
+
+        //this displays the text
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        //series-title-issuenumber.jpg is the format 
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + "jpg";
+            }
+        }
     }
 }
