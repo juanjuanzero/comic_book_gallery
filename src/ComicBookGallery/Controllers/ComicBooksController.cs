9891785753463d1273f9f5ecbdf8af7e955bada3
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-10 18:10:56 -0400

    added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +10,22 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller //added this to inherit from MVC
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController() //contructor for ComicBooksRepo, note constructors dont have a return type
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+
+        public ActionResult Detail(int? id) //adding the ? after int makes it nullable
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] //start with instanciating the array
-                {
-                    new Artist() {Name = "Dan Slott", Role = "Script" }, //then add an Artist model property that would be inside the array for a total of 5 elements
-                    new Artist() {Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() {Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() {Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
 
             return View(comicBook); //passed the comicBook model instance in the view method call to make the view to be strongly typed.
         }//adding an action method.. performing an action required to prepare a response for request
