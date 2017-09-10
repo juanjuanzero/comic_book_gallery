8bf0abcbdae57a925c6be87b7a4f821312d29c33
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-10 07:38:21 -0400

    updated comic book detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,22 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[] //start with instanciating the array
+                {
+                    new Artist() {Name = "Dan Slott", Role = "Script" }, //then add an Artist model property that would be inside the array for a total of 5 elements
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters" }
+                }
             };
 
-            return View();
+            return View(comicBook); //passed the comicBook model instance in the view method call to make the view to be strongly typed.
         }//adding an action method.. performing an action required to prepare a response for request
     }
 }
--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -28,8 +28,7 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-")
-                    .ToLower() + "-" + IssueNumber + "jpg";
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + "jpg";
             }
         }
     }
